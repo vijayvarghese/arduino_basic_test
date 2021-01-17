34996cc2ea6fe0eb8ee172c487186affeee0f7ff
Entity46 <33589052+vijayvarghese@users.noreply.github.com>
2021-01-17 08:12:57 +0530

    Add files via upload

---
+++ ultrasonic/ultrasonic.ino
@@ -0,0 +1,31 @@
+#define echoPin 2 // attach pin 2 Arduino to pin Echo of the Ultrasonic Sensor
+#define trigPin 3 //attach pin 3 Arduino to pin Trig of the Ultrasonic Sensor
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.println("Ultrasonic Sensor Test"); // print some text in Serial Monitor
+ 
+}
+void loop() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (two way)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+}
